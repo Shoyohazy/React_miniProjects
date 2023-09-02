078564ec7d5bac526bf4bf75ccf30a1b702212d2
Shubham Thakur <shubhamdthakur55@gmail.com>
2023-09-03 01:03:58 +0530

    using props to pass data

--- food_app/src/App.js
+++ food_app/src/App.js
@@ -31,7 +31,7 @@ const Navbar = () =>{
 };
 
 const restaurantList = [
-     {
+   {                                                                   
       "id": "37969",
       "name": "Burger King",
       "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
@@ -121,20 +121,317 @@ const restaurantList = [
         
       }
   },
-
+   {
+      "id": "355570",
+      "name": "Wow! Momo",
+      "cloudinaryImageId": "0984acc0ed7b914206dbbcb90297becc",
+      "locality": "Sector 4",
+      "areaName": "Kharghar",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "Tibetan",
+        "Healthy Food",
+        "Asian",
+        "Chinese",
+        "Snacks",
+        "Continental",
+        "Desserts",
+        "Beverages"
+      ],
+      "avgRating": 3.9,
+      "feeDetails": {
+        "restaurantId": "355570",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 7400
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 7400
+      },
+      "parentId": "1776",
+      "avgRatingString": "3.9",
+      "totalRatingsString": "1K+",
+      "sla": {
+        "deliveryTime": 26,
+        "lastMileTravel": 5,
+        "serviceability": "SERVICEABLE",
+        "slaString": "26 mins",
+        "lastMileTravelString": "5.0 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-09-03 03:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "50% OFF",
+        "subHeader": "UPTO ₹100"
+      },
+      "loyaltyDiscoverPresentationInfo": {
+        "badgeType": "BADGE_TYPE_OFFER_PACK",
+        "offerSubText": "with offer pack",
+        "nonSwiggyOneFreedelMessage": "FREE DELIVERY"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+  },
+   {
+      "id": "239444",
+      "name": "Pop Tate's",
+      "cloudinaryImageId": "x6xmtwtvaizieqwevoqy",
+      "locality": "Panvel",
+      "areaName": "Panvel",
+      "costForTwo": "₹600 for two",
+      "cuisines": [
+        "Chinese",
+        "Continental",
+        "Italian"
+      ],
+      "avgRating": 4.2,
+      "feeDetails": {
+        "restaurantId": "239444",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 6200
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 6200
+      },
+      "parentId": "1264",
+      "avgRatingString": "4.2",
+      "totalRatingsString": "500+",
+      "sla": {
+        "deliveryTime": 29,
+        "lastMileTravel": 4,
+        "serviceability": "SERVICEABLE",
+        "slaString": "29 mins",
+        "lastMileTravelString": "4.0 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-09-03 01:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "20% OFF",
+        "subHeader": "UPTO ₹50"
+      },
+      "loyaltyDiscoverPresentationInfo": {
+        "badgeType": "BADGE_TYPE_OFFER_PACK",
+        "offerSubText": "with offer pack",
+        "nonSwiggyOneFreedelMessage": "FREE DELIVERY"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+  },
+  {
+      "id": "343246",
+      "name": "Starbucks Coffee",
+      "cloudinaryImageId": "4df3497f1460dfd1ecd8125222f6d362",
+      "locality": "Sector 15",
+      "areaName": "Cbd Belapur",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Beverages",
+        "Cafe",
+        "Snacks",
+        "Desserts",
+        "Bakery",
+        "Ice Cream"
+      ],
+      "avgRating": 4.4,
+      "feeDetails": {
+        "restaurantId": "343246",
+        "fees": [
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 12900
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 12900
+      },
+      "parentId": "195515",
+      "avgRatingString": "4.4",
+      "totalRatingsString": "1K+",
+      "sla": {
+        "deliveryTime": 29,
+        "lastMileTravel": 9.8,
+        "serviceability": "SERVICEABLE",
+        "slaString": "29 mins",
+        "lastMileTravelString": "9.8 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-09-03 02:00:00",
+        "opened": true
+      },
+      "badges": {
+        "textExtendedBadges": [
+          {
+            "iconId": "guiltfree/GF_Logo_android_3x",
+            "shortDescription": "options available",
+            "fontColor": "#7E808C"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "",
+                  "fontColor": "#7E808C",
+                  "iconId": "guiltfree/GF_Logo_android_3x",
+                  "shortDescription": "options available"
+                }
+              }
+            ]
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "20% OFF",
+        "subHeader": "UPTO ₹50"
+      },
+      "loyaltyDiscoverPresentationInfo": {
+        "badgeType": "BADGE_TYPE_OFFER_PACK",
+        "offerSubText": "with offer pack",
+        "nonSwiggyOneFreedelMessage": "FREE DELIVERY"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      }
+  }
 ];
 
 
-const RestaurantCard = () =>{
+const RestaurantCard = ({name , locality , costForTwo , cloudinaryImageId , areaName}) =>{
   return(
     <div className = "restaurant-list">
         <img 
-        src = {"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + restaurantList[0].cloudinaryImageId}
+        alt = "logo"
+        src = {"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}
         />
-        <h2>{restaurantList[0].name}</h2>
-        <h2>{restaurantList[0].locality}</h2>
-        <h2>{restaurantList[0].cuisines.join(",")}</h2>
-        <h2>{restaurantList[0].cuisines.join(",")}</h2>
+        <h2>{name}</h2>
+        <h2>{locality}</h2>
+        <h2>{areaName}</h2>
+        <h2>{costForTwo}</h2>
+       
     </div>
   )
 }
@@ -142,14 +439,10 @@ const RestaurantCard = () =>{
 const Body = () =>{
     return(
       <div className="restaurant-card">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        <RestaurantCard {...restaurantList[0]} />
+        <RestaurantCard {...restaurantList[1]} />
+        <RestaurantCard {...restaurantList[2]} />
+        <RestaurantCard {...restaurantList[3]} />
       </div>
       
     )
