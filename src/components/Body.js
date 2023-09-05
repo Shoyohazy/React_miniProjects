0313290b46b4d6060c0a8534f67453931435beff
Shubham Thakur <shubhamdthakur55@gmail.com>
2023-09-05 23:59:15 +0530

    search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../Constants";
+import {useState} from "react";
+
+function filterData(searchText , restaurant){
+  
+  const filterData =restaurant.filter((restaurant)=>
+    restaurant.name.includes(searchText)
+  );
+  return filterData;
+}
 
 const Body = () =>{
+  const [restaurant , setRestaurant] = useState(restaurantList);
+  const [searchText , setSearchText] = useState("");
+
     return(
-      <div className="restaurant-card">
-        {restaurantList.map((restaurantList) =>{
-          return <RestaurantCard {...restaurantList} key = {restaurantList.id} />
-        })}
-      </div>
-      
+      <>
+        <div className = "search-bar">
+            <input 
+            type="text" 
+            value={searchText}
+            onChange={(e)=>{
+              setSearchText(e.target.value)
+            }}
+            />
+            <button onClick={()=>{
+              const data = filterData(searchText , restaurant)
+              setRestaurant(data)
+
+            }}>Search</button>
+        </div>
+        <div className="restaurant-card">
+          {restaurant.map((restaurantList) => {
+            return <RestaurantCard {...restaurantList} key={restaurantList.id} />
+          })}
+        </div>
+      </>
+
     )
 }
 
